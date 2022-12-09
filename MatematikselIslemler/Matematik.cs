bf0550a90f6a98e1d307572860ad6d1323c2d8d8
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-09 20:15:08 +0300

    first commit

---
+++ MatematikselIslemler/Matematik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikselIslemler
+{
+    public class Matematik
+    {
+        // topla
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc=sayi1 + sayi2;
+            return sonuc;
+        }
+
+        // cikart
+        public decimal cikartmaİslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        // bolme
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        // carpma
+        public decimal carpmaIslemi(decimal sayi1,decimal sayi2)
+        {
+            decimal sonuc=sayi1 * sayi2;
+            return sonuc;
+        }
+
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("****Menü****");
+            Console.WriteLine("1-Toplama");
+            Console.WriteLine("2-Çıkartma");
+            Console.WriteLine("3-Çarpma");
+            Console.WriteLine("4-Bölme");
+            Console.Write("Lütfen bir değer seçiniz : ");
+        }
+
+        public void sonucekranayaz(decimal kullanicisayi1, decimal kullanicisayi2, decimal sonuc, string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}",kullanicisayi1,operators,kullanicisayi2,sonuc);
+        }
+
+    }
+}
---
+++ MatematikselIslemler/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikselIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematik M = new Matematik();
+
+            YenidenIslemYap:
+
+            M.menuHazirla();
+            int kullaniciSecim = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Lütfen 1. sayı değerini giriniz : ");
+            decimal kullaniciSayi1=Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Lütfen 2. sayı değerini giriniz : ");
+            decimal kullaniciSayi2=Convert.ToDecimal(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch (kullaniciSecim)
+            {
+                case 1: // toplama
+                    sonuc=M.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucekranayaz(kullaniciSayi1, kullaniciSayi2,sonuc,"+");
+                    break;
+                case 2:// çıkartma
+                    sonuc=M.cikartmaİslemi(kullaniciSayi1,kullaniciSayi2);
+                    M.sonucekranayaz(kullaniciSayi1,kullaniciSayi2,sonuc,"-");
+                    break;
+                case 3:// çarpma
+                    sonuc = M.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucekranayaz(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                    break;
+                case 4:// bölme
+                    sonuc = M.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucekranayaz(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                    break;
+                default:
+                    Console.WriteLine("Belirtmiş olduğunuz değer bulunamadı !");
+                    Console.WriteLine("Lütfen yeniden deneyiniz ");
+                    System.Threading.Thread.Sleep(2000);
+                    goto YenidenIslemYap;
+            }
+
+            Console.WriteLine("Yeni işlem yapmak istiyor musunuz? [E/H]");
+            string EH=Console.ReadLine();
+
+            if (EH =="E")
+            {
+                goto YenidenIslemYap;
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
